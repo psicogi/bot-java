70667d1aa4e0a4a82339ff0716d4069fae6739f5
Giovania Dantas <fabianagiovana321@gmail.com>
2026-05-12 10:50:35 -0300

    feat: estrutura inicial do bot configurada + bot ja funcionando no telegram

---
+++ src/main/java/com/bot/vagas/TelegramBot.java
@@ -0,0 +1,51 @@
+package com.bot.vagas;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String mensagemRecebida = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            if (mensagemRecebida.equals("/start")) {
+                responder(chatId, "Oii! Eu sou o Spidey Bot! Em breve te mandarei algumas vagas em Java :)");
+            }
+        }
+    }
+
+    private void responder(long chatId, String texto) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(texto);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
